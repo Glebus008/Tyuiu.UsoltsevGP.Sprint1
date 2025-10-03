727535bc24b4513fc458ca6b1921ea2003e47b15
Глеб Усольцев <glebus008@gmail.com>
2025-10-03 15:45:40 +0500

    Task4

---
+++ Tyuiu.UsoltsevGP.Sprint1.Task4.V27.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.UsoltsevGP.Sprint1.Task4.V27.Lib
+{
+    public class DataService : ISprint1Task4V27
+{
+    public double Calculate(double x, double y)
+    {
+        var res = Math.Round((1 + (Math.Sin(Math.PI * x))) / (x - Math.Sqrt(Math.Abs(y))));
+        return res;
+    }
+}
+}
